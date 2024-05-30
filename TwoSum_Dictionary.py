6f7b507879e6a8bc47a7214c2f9b515f2ea73db7
Anugya Sahu <92376737+AnugyaSahu@users.noreply.github.com>
2024-05-30 19:48:59 +0200

    Added description

--- TwoSum_Dictionary.py
+++ TwoSum_Dictionary.py
@@ -1,4 +1,7 @@
-
+"""
+Given an array of integers nums and an integer target, return indices of the two numbers such that they add up to target.
+You may assume that each input would have exactly one solution, and you may not use the same element twice.
+"""
 class Solution:
     def twoSum(self, nums: list[int], target: int) -> list[int]:
       #Dictionary for easy retrieval using keys and O(1) time complexity for lookups, insertions, deletions
